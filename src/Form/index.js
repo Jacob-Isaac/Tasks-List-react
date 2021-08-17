4dbeba8749f968ff29de45bceb20583ce83ce7ea
Jakub Nowakowski <nowakowski.jakob@gmail.com>
2021-08-17 16:56:04 +0200

    Form section added with .css

---
+++ src/Form/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+const Form = () => (
+  <form className="section__form">
+    <input
+      autofocus
+      className="section__form--input"
+      placeholder=" Co jest do zrobienia?"
+    />
+    <button className="section__form--button">Dodaj zadanie</button>
+  </form>
+);
+
+export default Form;
