3734d107af4fb6e0ae99f2697403dcb3a8a842cd
Jakub Nowakowski <nowakowski.jakob@gmail.com>
2021-08-17 16:57:13 +0200

    Tasks section added with .css

---
+++ src/Tasks/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./style.css";
+
+const Tasks = (props) => (
+  <ul className="section__list">
+    {props.tasks.map((task) => (
+      <li
+        key={task.id}
+        className={`section__list--item${
+          task.done && props.hideShowTasks ? " section__list--hidden" : ""
+        }`}
+      >
+        <button className="button button__remove">🗑</button>
+        <button className="button button__done">{task.done ? "✔" : ""}</button>
+        <span
+          className={`content
+      ${task.done ? "content--decoration" : ""}`}
+        >
+          {task.content}
+        </span>
+      </li>
+    ))}
+  </ul>
+);
+
+export default Tasks;
